d7618258f279396546b113a608ea38ee3f972f23
Mariani88 <pm_mariani@hotmail.com>
2017-03-15 22:15:44 -0300

    configuracion inicial del proyecto

---
+++ proc-de-imagenes/src/main/java/untref/PrincipalGraphicInterface.java
@@ -0,0 +1,18 @@
+package untref;/**
+ * Created by matias on 14/03/17.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PrincipalGraphicInterface extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+
+	}
+}
